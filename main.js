0d3f210f68736a2364faeead8decc13c8da96fad
Jelika <smirnova.lika.23092001@gmail.com>
2020-10-31 17:01:27 +0300

    fix: urls tabs

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ var UXD441 = {
         oldFilterSectionSelector: '.subcategory-item__body',
         activeOldFiltersSelector: '.se_checkbox_is-active_span',
         tabsSelector: '.uxd-441-category-item__head',
+        resultsListSelector: '.results:last-child',
+        filterBlockSelector: '.filter-lvl1',
+        categoriesHeaderSelector: '.categories-header',
+
     },
 
     constants: {
@@ -18,6 +22,9 @@ var UXD441 = {
         this.nodes = {
             allResultsContent: document.querySelector(this.selectors.allResultsContentSelector),
             allOldFilters: document.querySelector(this.selectors.oldFilterSectionSelector).firstChild.childNodes,
+            resultsList: document.querySelector(this.selectors.resultsListSelector),
+            filterBlock: document.querySelector(this.selectors.filterBlockSelector),
+            categoriesHeader: document.querySelector(this.selectors.categoriesHeaderSelector),
         };
     },
 
@@ -26,9 +33,13 @@ var UXD441 = {
         this.nodes.tabsContainer.className = 'uxd-441-section-tabs';
         this.nodes.allTabsContainer = document.createElement('div');
         this.nodes.allTabsContainer.className = 'tabs-container';
-        this.nodes.allTabsContainer.addEventListener('click', function(event){this.tabsClick(event.target);}.bind(this));
+        this.nodes.allTabsContainer.addEventListener('click', function (event) { this.tabsClick(event.target); }.bind(this));
         this.nodes.tabsContainer.append(this.nodes.allTabsContainer);
         if (!document.querySelector('.tabs-container')) { this.nodes.allResultsContent.insertAdjacentElement('beforebegin', this.nodes.tabsContainer); }
+    },
+    hideFilterContainer: function () {
+        this.nodes.filterBlock.classList.add('uxd-441-hidden');
+        this.nodes.categoriesHeader.classList.add('uxd-441-hidden');
     },
     createTabs: function () {
         this.constants.tabsArray.forEach(function (elem, index) {
@@ -36,98 +47,90 @@ var UXD441 = {
             var tabItemInner = document.createElement('h6');
             tabItemInner.className = 'uxd-441-category-item__heading';
             tabItemInner.innerText = elem;
-            if (index) {
-                tabItem.className = 'uxd-441-category-item__head';
+            if (!sessionStorage.getItem('currentFilter')) {
+                sessionStorage.setItem('currentFilter', "Alle Seite");
             }
-            else { tabItem.className = 'uxd-441-category-item__head active'; }
+            if (elem === sessionStorage.getItem('currentFilter')) {
+                tabItem.className = 'uxd-441-category-item__head active';
+            }
+            else { tabItem.className = 'uxd-441-category-item__head'; }
+
             tabItem.append(tabItemInner);
             this.nodes.allTabsContainer.insertAdjacentElement('beforeend', tabItem);
         }.bind(this));
     },
 
     tabsClick: function (clickedElement) {
-        debugger
         var filterHeader = clickedElement.innerText;
-        document.querySelectorAll(this.selectors.tabsSelector).forEach(function (elem) {
-            elem.classList.remove('active');
-        });
-        document.querySelectorAll(this.selectors.activeOldFiltersSelector).forEach(function (activeElem) {
-            activeElem.click();
-        });
+        var searchText = document.querySelector('.sdl-header-se_search-field').value;
+        var urlString = '';
         if (!clickedElement.classList.contains('active') && !clickedElement.parentNode.classList.contains('active')) {
-
             if (clickedElement.classList.contains('uxd-441-category-item__head')) {
                 clickedElement.classList.add('active');
             } else { clickedElement.parentNode.classList.add('active'); }
-            console.log(clickedElement.innerText);
+
             switch (filterHeader) {
                 case "Alle Seite":
+                    urlString = 'https://www.se.com/de/de/search/' + searchText + '?multifilter=';
+                    location.assign(urlString);
                     break;
                 case "Produkte":
-    
-                    document.querySelector(this.selectors.oldFilterSectionSelector).firstChild.childNodes.forEach(function (elem) {
-                        
-                        if (elem.textContent.replace(/\(([^)]+)\)/g, "").trim() === "Produktbereiche" || elem.textContent.replace(/\(([^)]+)\)/g, "").trim() === "Datenblätter") {
-                         console.log(elem.textContent.replace(/\(([^)]+)\)/g, "").trim());
-                            elem.childNodes[0].click();
-                        }
-                    }.bind(this));
-
+                    urlString = 'https://www.se.com/de/de/search/' + searchText + '?multifilter=Produktbereiche%2CDatenblätter';
+                    location.assign(urlString);
                     break;
                 case "Unterlagen":
-                    document.querySelector(this.selectors.oldFilterSectionSelector).firstChild.childNodes.forEach(function (elem) {
-                        if (elem.textContent.replace(/\(([^)]+)\)/g, "").trim() === "CAD & Zeichnung" || elem.textContent.replace(/\(([^)]+)\)/g, "").trim() === "Kataloge und Benutzerhandbücher" || elem.textContent.replace(/\(([^)]+)\)/g, "").trim() === "Technische Informationen") {
-                            elem.childNodes[0].click();
-                        }
-                    }.bind(this));
-
+                    urlString = 'https://www.se.com/de/de/search/' + searchText + '?multifilter=CAD+%26+Zeichnung%2CKataloge+und+Benutzerhandbücher%2CTechnische+Informationen';
+                    location.assign(urlString);
                     break;
                 case "Software":
-                    document.querySelector(this.selectors.oldFilterSectionSelector).firstChild.childNodes.forEach(function (elem) {
-
-                        if (elem.textContent.replace(/\(([^)]+)\)/g, "").trim() === "Software/Firmware") {
-                            elem.childNodes[0].click();
-                        }
-                    }.bind(this));
-
+                    urlString = 'https://www.se.com/de/de/search/' + searchText + '?multifilter=Software%2FFirmware';
+                    location.assign(urlString);
                     break;
                 case "FAQ":
-                    document.querySelector(this.selectors.oldFilterSectionSelector).firstChild.childNodes.forEach(function (elem) {
-                        if (elem.textContent.replace(/\(([^)]+)\)/g, "").trim() === "Support und FAQs") {
-                            elem.childNodes[0].click();
-                        }
-                    }.bind(this));
-
+                    urlString = 'https://www.se.com/de/de/search/' + searchText + '?multifilter=Support+und+FAQs';
+                    location.assign(urlString);
                     break;
                 default:
                     break;
             }
         }
+        sessionStorage.setItem('currentFilter', filterHeader);
     },
-    // setObserverToResultList: function () {
-    //     var addClassesToResultCards = this.addClassesToResultCards.bind(this);
-    //     var observer = new MutationObserver(addClassesToResultCards);
 
-    //     observer.observe(this.nodes.resultsList, {
-    //         childList: true,
-    //         subtree: true,
-    //     });
-    // },
+    addClassesToResultCards: function () {
+        this.nodes.resultsList
+            .querySelectorAll(this.selectors.resultCardsSelector)
+            .forEach(function (card) {
+                card.classList.add('uxd-441-primary');
+            });
+    },
+
+    setObserverToResultList: function () {
+        var addClassesToResultCards = this.addClassesToResultCards.bind(this);
+        var observer = new MutationObserver(addClassesToResultCards);
+
+        observer.observe(this.nodes.resultsList, {
+            childList: true,
+            subtree: true,
+        });
+    },
 
     init: function () {
         if (document.readyState !== 'loading') {
             this.getNodes();
+            this.hideFilterContainer();
             this.initTabContainerSection();
             this.createTabs();
-            // this.setObserverToResultList();
+            this.setObserverToResultList();
         } else {
             var _this = this;
 
             window.addEventListener('load', function () {
                 _this.getNodes();
+                _this.hideFilterContainer();
                 _this.initTabContainerSection();
                 _this.createTabs();
-                // this.setObserverToResultList();
+                _this.setObserverToResultList();
             });
         }
     },
